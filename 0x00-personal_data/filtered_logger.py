2fe2828e52b433cef3a6da6103eb2ebfd0b4d8e6
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-05-17 04:40:55 +0200

    filter log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
 
 import re
+import logging
+from datetime import datetime
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: tuple):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage().split(self.SEPARATOR)
+        filtered = filter_datum(
+            self.fields,
+            self.REDACTION,
+            f"{self.SEPARATOR} ".join(message),
+            self.SEPARATOR,
+        )
+        timestamp = datetime.utcfromtimestamp(record.created).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        return f"[HOLBERTON] {record.name} INFO {timestamp} {filtered}"
 
 
 def filter_datum(fields, redaction, message, separator):
     message_list = message.split(separator)
     for field in message_list:
         if field.split("=")[0] in fields:
-            index = message_list.index(field)
-            message_list[index] = re.sub(
+            message_list[message_list.index(field)] = re.sub(
                 "^" + field, field.split("=")[0] + "=" + redaction, field
             )
 
